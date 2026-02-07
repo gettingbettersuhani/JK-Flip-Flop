7b15d70b07df76f194ba30915151ee6394509445
gettingbettersuhani <suhanipareek01@gmail.com>
2026-02-08 01:24:40 +0530

    Create Constraint(Push button).v

---
+++ Constraint(Push button).v
@@ -0,0 +1,15 @@
+set_property CLOCK_DEDICATED_ROUTE FALSE [get_nets clk_IBUF]
+set_property PACKAGE_PIN N17 [get_ports {clk}]
+set_property IOSTANDARD LVCMOS33 [get_ports {clk}]
+
+set_property PACKAGE_PIN J15 [get_ports {j}]
+set_property IOSTANDARD LVCMOS33 [get_ports {j}]
+
+set_property PACKAGE_PIN L16 [get_ports {k}]
+set_property IOSTANDARD LVCMOS33 [get_ports {k}]
+
+set_property PACKAGE_PIN H17 [get_ports {q}]
+set_property IOSTANDARD LVCMOS33 [get_ports {q}]
+
+set_property PACKAGE_PIN K15 [get_ports {qbar}]
+set_property IOSTANDARD LVCMOS33 [get_ports {qbar}]
